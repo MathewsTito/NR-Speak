5320fce50814b6391b4d762ad9ff3a443bb971f2
Tito Mathews <tito.mathews@gmail.com>
2014-09-23 21:08:44 -0600

    initial code

---
+++ README.md
@@ -0,0 +1,16 @@
+speak
+=======
+
+Node-RED module to support text to speech
+
+Speaks out the content of message.payload
+
+Pre-requesites
+--------------
+1. mplayer (install using sudo apt-get install mplayer)
+
+
+
+To install the node, edit install.sh and set NR_HOME to 
+the directory containing your Node-RED installation. Subsequently
+execute install.sh
---
+++ speak.js
@@ -0,0 +1,26 @@
+
+
+var say = require('say');
+var sys = require('sys');
+var exec = require('child_process').exec;
+var child;
+
+
+module.exports = function(RED){
+    function Speak(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+        this.on('input', function(msg){
+            var text = msg.payload;
+            var command = "speech.sh "+text;
+            function puts(error,stdout,stderr) { sys.puts(stdout) }
+            exec(command);
+            //say.speak('Tito',text);
+            node.send(msg);
+        });
+
+    }
+    
+    RED.nodes.registerType("speak",Speak);
+
+}
